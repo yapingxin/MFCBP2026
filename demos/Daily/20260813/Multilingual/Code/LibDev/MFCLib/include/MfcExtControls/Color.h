612f8615df1d79b2f1d1394d1696dd5307664e17
Yaping Xin <155618073+XinYaping@users.noreply.github.com>
2026-08-14 10:10:47 +0800

    Add static MFC library: MfcExtControls
    LibDev\MFCLib\MfcExtControls\MfcExtControls.vcxproj

---
+++ demos/Daily/20260813/Multilingual/Code/LibDev/MFCLib/include/MfcExtControls/Color.h
@@ -0,0 +1,21 @@
+﻿#ifndef _INC_LIBDEV_MFCLIB_MFCEXTCONTROLS_Color_H
+#define _INC_LIBDEV_MFCLIB_MFCEXTCONTROLS_Color_H
+
+// Color.h
+// Colorref's to use with your Programs
+
+#define RED        RGB(127,  0,  0)
+#define GREEN      RGB(  0,127,  0)
+#define BLUE       RGB(  0,  0,127)
+#define LIGHTRED   RGB(255,  0,  0)
+#define LIGHTGREEN RGB(  0,255,  0)
+#define LIGHTBLUE  RGB(  0,  0,255)
+#define BLACK      RGB(  0,  0,  0)
+#define WHITE      RGB(255,255,255)
+#define GRAY       RGB(192,192,192)
+#define YELLOW     RGB(255,255,  0)
+
+#define CONTROLBG   RGB(225,225,225)    // 默认风格默认控件背景色
+#define CTLTXTCOLOR RGB( 51, 51, 51)    // 默认风格默认文字前景色
+
+#endif // !_INC_LIBDEV_MFCLIB_MFCEXTCONTROLS_Color_H
---
+++ demos/Daily/20260813/Multilingual/Code/LibDev/MFCLib/src/ColorEdit/ColorEdit.md
@@ -0,0 +1,3 @@
+﻿Using Colors in CEdit and CStatic
+https://www.codeproject.com/articles/1035/using-colors-in-cedit-and-cstatic
+
